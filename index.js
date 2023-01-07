3a929ad156bc389b960d898ea2ba2d74d09e98ee
Armando Peña Tamayo (Ender) <ender@aleph.engineering>
2023-01-06 19:24:46 -0500

    Adding logger for new problems to general README

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
-const { exec, execSync } = require('child_process');
+const { execSync } = require('child_process');
+const { colors, formats } = require('./consoleFormatter');
 
 const categories = {
   'ps': 'problemSolving',
@@ -41,25 +42,41 @@ function ${name}(){
 console.log(${name}());`;
 }
 
+
+
 (function main() {
   const args = process.argv.slice(2);
   const category = categories[args[0]] || 'Random';
   const difficulty = difficulties[args[1]] || 'Random';
   const name = args[2] || 'Random';
-  const exerciseFile = `./${category}/${difficulty}/${name}`;
+  const firstLetter = fn => s => s.charAt(0)[fn]() + s.slice(1);
+  const problemName = firstLetter('toUpperCase')(name.replace(/\-/g, ' '));
+  const funcName = firstLetter('toLowerCase')(name.split('-').join(''));
+  const exerciseFile = `./${category}/${difficulty}/${funcName}`;
+
+  // Creates directory tree.
   fs.mkdirSync(exerciseFile, { recursive: true }, (err) => {
     if (err) throw new Error(err);
-    console.log(`Exercise file created here: ${exerciseFile}`);
   });
-  fs.writeFileSync(`${exerciseFile}/README.md`, createReadme({ name, difficulty }), err => {
-    if (err) throw new Error(err);
-    console.log(`README file created! Please fill in the blanks.`);
-  });
-  fs.writeFileSync(`${exerciseFile}/${name}.js`, createJSFile({ name }), err => {
-    if (err) throw new Error(err);
-    console.log(`${name}.js created! Please finish function ${name}().`);
-  });
-  execSync(`code ${exerciseFile}/README.md && code ${exerciseFile}/${name}.js`, (err, stdout, stderr) => {
+  // Creates problem README file based on a template.
+  fs.writeFileSync(`${exerciseFile}/README.md`, createReadme({ name: problemName, difficulty }), err => {
     if (err) throw new Error(err);
   });
-})()
+  // Creates problem solution JS file based on a template.
+  fs.writeFileSync(`${exerciseFile}/${funcName}.js`, createJSFile({ name: funcName }), err => {
+    if (err) throw new Error(err);
+  });
+  // Updates common README file with the new problem set.
+  fs.appendFileSync(`./README.md`, `* **${problemName}** ${category} *${difficulty}* \n`, err => {
+    if (err) throw new Error(err);
+  });
+  // Open each file in the vscode.
+  execSync(`code README.md && code ${exerciseFile}/README.md && code ${exerciseFile}/${funcName}.js`, (err, stdout, stderr) => {
+    if (err) throw new Error(err);
+  });
+
+  // CLI response
+  console.log(colors.bg.green(formats.bold(' Finished! ')));
+  console.log(`${colors.f.green('Problem set files created here')} ${exerciseFile}`);
+  console.log(colors.f.yellow('Remember filling the blanks at the problem README!'));
+})() //  main IIFE
